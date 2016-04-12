65327c8a9e9f013ce015142a31278761a5cd37c5
Alex Podworny <alex.podworny@hotmail.com>
2016-04-12 16:47:04 -0500

    Merge before adding git desktop

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Scrape me some data</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/css/main.css">
   </head>
   <body>
     <?php
